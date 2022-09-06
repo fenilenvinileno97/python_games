0a3b6021eea2f3b1fd816b2f9dba8881c867eabf
Marlon Marín <marlonmariin97@outlook.com>
2022-09-05 19:54:31 -0500

    Challenge to create a squared function of natural numbers in a list

---
+++ squared_numbers.py
@@ -0,0 +1,10 @@
+def run():
+    natural_nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    squared = 0
+
+    for i in natural_nums:
+        squared = i**2
+        print(squared)
+
+if __name__ == '__main__':
+    run() 
