10f66bd83fa0c4b47908d55a075c45eee199fa79
Marlon Marín <marlonmariin97@outlook.com>
2022-09-06 19:16:07 -0500

    Gio challenge

---
+++ gio_challenge.py
@@ -0,0 +1,12 @@
+#Reto para transformar tipo_de_entidad en entidades
+tipo_de_entidad = {
+"a": "A",
+"b": "B",
+"c": "C"
+}
+
+entidades = [
+    {"label" : "a", "id": "A"},
+    {"label" : "b", "id": "B"},
+    {"label" : "c", "id": "C"},
+]
