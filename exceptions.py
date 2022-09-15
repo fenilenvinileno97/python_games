9ccf8ed2b4b333ffca062af9ea02efef83c9823a
Marlon Marín <marlonmariin97@outlook.com>
2022-09-14 21:35:36 -0500

    Changes in print(palindrome()) function

--- exceptions.py
+++ exceptions.py
@@ -6,13 +6,13 @@ def palindrome(string):
         if len(string) == 0:
             raise ValueError('You have to write something')
         return string == string[::-1]
-    except ValueError as ve:
+    except ValueError as ve: #This is the exception
         print(ve)
         return False
 
 def run():
     try:
-        print(palindrome(str(input('Enter a sentence: '))))
+        print(palindrome(str(input('Enter a sentence: \n'))))
     except TypeError:
         print('Just strings are allowed')
 
