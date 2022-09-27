0ed8d326f6800e3f46fc3c4810b74378eef5374d
Marlon Marín <marlonmariin97@outlook.com>
2022-09-26 21:43:55 -0500

    Correction: read() function was not called in run() and entry point

--- files.py
+++ files.py
@@ -1,6 +1,6 @@
 def read():
     numbers = []
-    with open("./files/numbers.txt", "r", endcoding= "utf-8") as f:
+    with open("./files/numbers.txt", "r", encoding = "utf-8") as f: #take care with " " characters
         for line in f:
             numbers.append(int(line))
     print(numbers)
@@ -9,7 +9,7 @@ def write():
     pass
 
 def run():
-    pass
+    read()
 
 if __name__ == '__main__':
     run()
