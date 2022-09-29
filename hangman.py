f74f5508a6c47dfbd19c1738bf59ad1fca7cbc00
Marlon Marín <marlonmariin97@outlook.com>
2022-09-28 22:47:43 -0500

    Hangman.py file creation for testing final project in Intermediate Python course

--- hangman.py
+++ hangman.py
@@ -8,6 +8,8 @@ def read(filepath='./files/data.txt'):
             words.append(line.strip().upper()) #strip is a method used removing spaces between strings
     print(words)
 def run():
-    read()
+    data = read(filepath='./files/data.txt')
+    chosen_word = random.choice(data)
+    print(chosen_word)
 if __name__ == '__main__':
     run()
