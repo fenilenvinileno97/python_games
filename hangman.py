9094a003743cf6f81cea54d238cd7193a1feb250
Marlon Marín <marlonmariin97@outlook.com>
2022-09-28 23:16:20 -0500

    Extracting a random word from a txt file

--- hangman.py
+++ hangman.py
@@ -5,11 +5,13 @@ def read(filepath='./files/data.txt'):
     words = []
     with open(filepath, 'r', encoding='utf-8') as f:
         for line in f:
-            words.append(line.strip().upper()) #strip is a method used removing spaces between strings
-    print(words)
+            words.append(line.strip().lower()) #strip is a method used removing spaces between strings
+    return words
+    
 def run():
     data = read(filepath='./files/data.txt')
     chosen_word = random.choice(data)
-    print(chosen_word)
+    chosen_word_list = [letter for letter in chosen_word.lower().strip()]
+    print(chosen_word_list)
 if __name__ == '__main__':
     run()
